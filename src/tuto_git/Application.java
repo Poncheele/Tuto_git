913a1762331b1430e68a6479b68e33b6419972a3
Clement Poncheele <cponchee@pcalb-mm0000.univ-jfc.fr>
2020-02-05 14:51:50 +0100

    ajout de titre

--- src/tuto_git/Application.java
+++ src/tuto_git/Application.java
@@ -19,6 +19,7 @@ public class Application extends Frame implements WindowListener {
 		Vue vue = new Vue();
 		modl.addObserver(vue);
 		vue.addActionListener(ctrl);
+		this.setTitle("titre de fenetre nul");
 		this.setLocationRelativeTo(null);
 		this.add(vue);
 		this.pack();
